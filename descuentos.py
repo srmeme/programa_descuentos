6c78f8c3bb6e509081b8e7b446d454718a063910
Emmanuel González <emmanuelfga87@gmail.com>
2022-08-26 12:39:15 -0600

    Creación de menu con cantidades y descuentos

--- descuentos.py
+++ descuentos.py
@@ -1,5 +1,14 @@
 def run():
-    pass
+    #pass
+    menu = """
+    Este programa le ayudará a conocer cuanto descuento puede aplicar en sus compras:
+    
+    1 a 250 unidades (0% de descuento)
+    251 a 500 unidades (5% de descuento)
+    501 a 1000 unidades (10% de descuento)
+    1001 unidades o más (15% de descuento)
+    """
+    print(menu)
 
 
 if __name__ == "__main__":
