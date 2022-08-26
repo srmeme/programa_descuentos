d44e922608cf206d3d8b1fc517cd7d3d0435ca07
Emmanuel González <emmanuelfga87@gmail.com>
2022-08-26 13:38:49 -0600

    Finalización programación logica de descuentos

--- descuentos.py
+++ descuentos.py
@@ -13,9 +13,13 @@ def run():
     print(menu)
     cantidad = int(input("¿Cuantas unidades desea adquirir?: "))
     if cantidad >= 1001:
-        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str((cantidad*35.28)*0.15))
+        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str(round((((cantidad*35.28)-(cantidad*35.28)*0.15)),2)))
     elif cantidad >=501<=1000:
-        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str(cantidad*35.28))
+        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str(round((((cantidad*35.28)-(cantidad*35.28)*0.10)),2)))
+    elif cantidad >=251<=500:
+        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str(round((((cantidad*35.28)-(cantidad*35.28)*0.05)),2)))
+    elif cantidad >=1<=250:
+        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str(round((((cantidad*35.28)-(cantidad*35.28)*0.00)),2)))
 
 
 if __name__ == "__main__":
