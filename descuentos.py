88571e9e25412ef69c93eb166911a58c2df6fc7f
Emmanuel González <emmanuelfga87@gmail.com>
2022-08-26 13:09:09 -0600

    Inicio programación logica de descuentos

--- descuentos.py
+++ descuentos.py
@@ -7,8 +7,15 @@ def run():
     251 a 500 unidades (5% de descuento)
     501 a 1000 unidades (10% de descuento)
     1001 unidades o más (15% de descuento)
+
+    Costo unitario $35.28
     """
     print(menu)
+    cantidad = int(input("¿Cuantas unidades desea adquirir?: "))
+    if cantidad >= 1001:
+        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str((cantidad*35.28)*0.15))
+    elif cantidad >=501<=1000:
+        print("La cantidad a pagar por " + str(cantidad) + " unidades es de $ " + str(cantidad*35.28))
 
 
 if __name__ == "__main__":
